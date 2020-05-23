98974f0342aa4ba1f0ba9c368fede9adf3a9cb4c
GilbertHG <gilbertgoes11@gmail.com>
2020-05-23 14:11:35 +0800

    Adapter Update

--- app/src/main/java/com/gilberthg/tasktodo/db/task/Task.kt
+++ app/src/main/java/com/gilberthg/tasktodo/db/task/Task.kt
@@ -9,6 +9,25 @@ data class Task(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
     val id: Int? = null,
-    @ColumnInfo(name = "task")
-    var task: String
+
+    @ColumnInfo(name = "title")
+    var title: String,
+
+    @ColumnInfo(name = "note")
+    var note: String? = null,
+
+    @ColumnInfo(name = "date_created")
+    var dateCreated: String,
+
+    @ColumnInfo(name = "date_updated")
+    var dateUpdated: String,
+
+    @ColumnInfo(name = "due_date")
+    var dueDate: String,
+
+    @ColumnInfo(name = "due_time")
+    var dueTime: String,
+
+    @ColumnInfo(name = "remind_me")
+    var remindMe: Boolean = true
 )
--- app/src/main/java/com/gilberthg/tasktodo/ui/TaskAdapter.kt
+++ app/src/main/java/com/gilberthg/tasktodo/ui/TaskAdapter.kt
@@ -2,10 +2,13 @@ package com.gilberthg.tasktodo.ui
 
 import android.content.Context
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.gilberthg.tasktodo.R
 import com.gilberthg.tasktodo.db.task.Task
+import kotlinx.android.synthetic.main.item_task.view.*
 
 class TaskAdapter(private val context: Context?,private val listener:(Task,Int)->Unit):RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
     private var taskList = listOf<Task>()
@@ -15,19 +18,31 @@ class TaskAdapter(private val context: Context?,private val listener:(Task,Int)-
         Log.d("cek",taskList.toString())
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder =
+        TaskViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task,parent,false))
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
-    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getItemCount(): Int = taskList.size
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int){
+        if(context!=null){
+            holder.bindItem(taskList[position],listener)
+        }
     }
 
     class TaskViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bindItem(task: Task, listener: (Task, Int) -> Unit){
+            val date = if(task.dateUpdated != task.dateCreated) "Updated at ${task.dateUpdated}" else "Created at ${task.dateCreated}"
+            val dueDate = "Due ${task.dueDate},${task.dueTime}"
 
+            itemView.tv_title.text = task.title
+            itemView.tv_detail.text = task.note
+            itemView.tv_due_date.text = dueDate
+            itemView.tv_created_date.text = date
+
+            itemView.setOnClickListener{
+                listener(task, layoutPosition)
+            }
+        }
     }
 }
