6e7c76b3ca293dfe419276b39e75fbdfa03394e2
GilbertHG <gilbertgoes11@gmail.com>
2020-05-23 14:20:12 +0800

    Upadate View Model

--- app/src/main/java/com/gilberthg/tasktodo/ui/TaskViewModel.kt
+++ app/src/main/java/com/gilberthg/tasktodo/ui/TaskViewModel.kt
@@ -1,7 +1,29 @@
 package com.gilberthg.tasktodo.ui
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.gilberthg.tasktodo.db.task.Task
+import com.gilberthg.tasktodo.db.task.TaskRepository
 
-class TaskViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class TaskViewModel(application: Application) : AndroidViewModel(application) {
+    private var taskRepository = TaskRepository(application)
+    private var tasks: LiveData<List<Task>>? = taskRepository.getTasks()
+
+    fun getTasks(): LiveData<List<Task>>? {
+        return tasks
+    }
+
+    fun insertTask(task: Task){
+        taskRepository.insert(task)
+    }
+
+    fun deleteTask(task: Task){
+        taskRepository.delete(task)
+    }
+
+    fun updateTask(task: Task){
+        taskRepository.update(task)
+    }
 }
