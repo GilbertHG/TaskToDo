bb6b8e8b4ec5e78e60d47f799872c7d6dff9d61d
GilbertHG <gilbertgoes11@gmail.com>
2020-05-23 15:20:56 +0800

    Rearrange MainActivity Component

--- app/src/main/java/com/gilberthg/tasktodo/ui/MainActivity.kt
+++ app/src/main/java/com/gilberthg/tasktodo/ui/MainActivity.kt
@@ -3,13 +3,17 @@ package com.gilberthg.tasktodo.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.gilberthg.tasktodo.R
+import com.gilberthg.tasktodo.db.task.Task
+import com.gilberthg.tasktodo.ui.utility.Commons
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var taskViewModel: TaskViewModel
     private lateinit var taskAdapter: TaskAdapter
+    private val taskList: ArrayList<Task> = ArrayList<Task>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,9 +21,43 @@ class MainActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this)
         rv.layoutManager = layoutManager
 
+        taskAdapter = TaskAdapter(this){task, _ ->
+            val options = resources.getStringArray(R.array.option_item)
+            Commons.showSelector(this, "Choose Action", options){_, i->
+                when(i){
+                    1 -> showDetailDialog(task)
+                    2 -> showUpdateDialog(task)
+                    3 -> showDeleteDialog(task)
+                }
+            }
+        }
+
+        rv.adapter = taskAdapter
+        taskViewModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+
+        swipe_refresh_layout.setOnRefreshListener {
+            refreshData()
+        }
+
         button_add.setOnClickListener{
             showInsertDialog()
         }
+    }
+
+    private fun refreshData() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun showDeleteDialog(task: Task) {
+
+    }
+
+    private fun showUpdateDialog(task: Task) {
+
+    }
+
+    private fun showDetailDialog(task: Task) {
+
     }
 
     private fun showInsertDialog() {
